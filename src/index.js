a7fdc4b908b326b75c106058fbea8e3e4501544d
Owan Hunte <owanh@hotmail.com>
2019-08-23 07:51:09 -0400

    wip Rendering user list in sidebar.

--- src/index.js
+++ src/index.js
@@ -27,6 +27,10 @@ io.on('connection', (socket) => {
 
             socket.emit('message', generateMessage('Admin', 'Welcome!'))
             socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
 
             callback()
         }
@@ -55,6 +59,10 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
 });
